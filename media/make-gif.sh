d28f270df4200f7da2fc185be2a526a9f761985b
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-09-14 13:51:53 -0700

    checking in gif

---
+++ media/make-gif.sh
@@ -0,0 +1,7 @@
+ffmpeg -y -i '/Volumes/DataPuddle3/web app.mov' \
+  -vf "scale=350:-1:flags=lanczos,format=rgb24,palettegen=max_colors=32" \
+  -frames:v 1 palette16.png
+
+ffmpeg -i '/Volumes/DataPuddle3/web app.mov' -i palette16.png \
+  -lavfi "fps=50,scale=350:-1:flags=lanczos[x];[x][1:v]paletteuse=dither=none" \
+  -loop 0 raptor.rl.tools.gif
